335ae46b629814686cf073156709586b189afb53
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-07-21 11:20:05 +0530

    moonfly is nice _true_ dark theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -108,6 +108,7 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "lunarvim/onedarker.nvim"
+  use "bluz71/vim-moonfly-colors"
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp" }
