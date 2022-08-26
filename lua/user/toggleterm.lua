009c337a6a9b02e6ac9a0b879d2572e639f3ab22
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-08-27 00:31:47 +0530

    No signcolumn for toggleterm in horizontal/vertical split

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -142,6 +142,7 @@ vim.api.nvim_set_keymap("i", "<m-1>", "<cmd>lua _FLOAT_TERM()<CR>", { noremap = 
 
 local vertical_term = Terminal:new {
   direction = "vertical",
+  vim.wo.signcolumn = "no"
   on_open = function(term)
     vim.cmd "startinsert!"
     vim.api.nvim_buf_set_keymap(
@@ -188,6 +189,7 @@ local horizontal_term = Terminal:new {
     },
   },
   on_open = function(term)
+    vim.wo.signcolumn = "no"
     vim.cmd "startinsert!"
     vim.api.nvim_buf_set_keymap(
       term.bufnr,
