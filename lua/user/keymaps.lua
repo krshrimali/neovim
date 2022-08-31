22fe4302d66f568498a674de38d302bf4f8e1ec1
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-08-31 21:53:22 +0530

    Add keymaps to move line up and down

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -62,6 +62,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Move text up and down
 -- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 -- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<S-f>", "<Esc>:m .+1<CR>", opts)
+keymap("n", "<S-u>", "<Esc>:m .-2<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
