4f6bd21a75310be37799abfb530f477a00e9e53c
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-02-23 07:53:09 +0530

    ignore_ft_on_setup is deprecated, TODO - fix this

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -38,11 +38,12 @@ nvim_tree.setup {
   -- disable_netrw = true,
   -- hijack_netrw = true,
   -- open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
+  -- TODO: Fix this it's commented out now
+  -- ignore_ft_on_setup = {
+  --   "startify",
+  --   "dashboard",
+  --   "alpha",
+  -- },
   -- filters = {
   --   custom = { ".git" },
   --   -- exclude = { ".gitignore" },
