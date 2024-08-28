0742e0040ab52d402de316ece0faefaa9bb4c4c8
Kushashwa Ravi Shrimali <krshrimali@Kushashwas-MacBook-Air.local>
2024-08-28 23:50:55 +0530

    don't disable lsp and syntax

--- lua/user/bigfile.lua
+++ lua/user/bigfile.lua
@@ -1,12 +1,22 @@
 require("bigfile").setup {
-  -- detect long python files
-  pattern = function(bufnr, filesize_mib)
-    -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
-    local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
-    local file_length = #file_contents
-    local filetype = vim.filetype.match({ buf = bufnr })
-    if file_length > 5000 and filetype == "python" then
-      return true
-    end
-  end
+    -- detect long python files
+    pattern = function(bufnr, filesize_mib)
+        -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+        local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+        local file_length = #file_contents
+        local filetype = vim.filetype.match({ buf = bufnr })
+        if file_length > 5000 and filetype == "python" then
+            return true
+        end
+    end,
+    features = { -- features to disable
+        "indent_blankline",
+        "illuminate",
+        -- "lsp",
+        "treesitter",
+        -- "syntax",
+        "matchparen",
+        "vimopts",
+        "filetype",
+    },
 }
