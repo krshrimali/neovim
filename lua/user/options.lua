a4a59e8bdfbc43788955daa13e440a3a0b8ac6ba
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-07-12 12:48:55 +0530

    init.lua cleanup

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,46 +1,46 @@
 local options = {
-  pumblend = 0,
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 0,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 100,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-  tabstop = 4,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  -- cursorlineopt = "number",
-  number = true,                           -- set numbered lines
-  laststatus = 3,
-  showcmd = true,
-  ruler = false,
-  relativenumber = false, -- set relative numbered lines
-  numberwidth = 4,        -- set number column width to 2 {default 4}
-  signcolumn = "yes",     -- always show the sign column, otherwise it would shift the text each time
-  wrap = true,            -- display lines as one long line
-  -- scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
-  guifont = "monospace:h17", -- the font used in graphical neovim applications
-  title = true,
-  -- colorcolumn = "80",
-  -- colorcolumn = "120",
+    pumblend = 0,
+    backup = false,                          -- creates a backup file
+    clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+    cmdheight = 1,                           -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0,                        -- so that `` is visible in markdown files
+    fileencoding = "utf-8",                  -- the encoding written to a file
+    hlsearch = true,                         -- highlight all matches on previous search pattern
+    ignorecase = true,                       -- ignore case in search patterns
+    mouse = "a",                             -- allow the mouse to be used in neovim
+    pumheight = 10,                          -- pop up menu height
+    showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+    showtabline = 0,                         -- always show tabs
+    smartcase = true,                        -- smart case
+    smartindent = true,                      -- make indenting smarter again
+    splitbelow = true,                       -- force all horizontal splits to go below current window
+    splitright = true,                       -- force all vertical splits to go to the right of current window
+    swapfile = false,                        -- creates a swapfile
+    termguicolors = true,                    -- set term gui colors (most terminals support this)
+    timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = true,                         -- enable persistent undo
+    updatetime = 100,                        -- faster completion (4000ms default)
+    writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    expandtab = true,                        -- convert tabs to spaces
+    shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+    tabstop = 4,                             -- insert 2 spaces for a tab
+    cursorline = true,                       -- highlight the current line
+    -- cursorlineopt = "number",
+    number = true,                           -- set numbered lines
+    laststatus = 3,
+    showcmd = true,
+    ruler = false,
+    relativenumber = false, -- set relative numbered lines
+    numberwidth = 4,        -- set number column width to 2 {default 4}
+    signcolumn = "yes",     -- always show the sign column, otherwise it would shift the text each time
+    wrap = true,            -- display lines as one long line
+    -- scrolloff = 8,                           -- is one of my fav
+    sidescrolloff = 8,
+    guifont = "monospace:h17", -- the font used in graphical neovim applications
+    title = true,
+    -- colorcolumn = "80",
+    -- colorcolumn = "120",
 }
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
@@ -49,14 +49,14 @@ local options = {
 -- vim.opt.fillchars = vim.opt.fillchars + "vertright: "
 vim.opt.fillchars = vim.opt.fillchars + "eob: "
 vim.opt.fillchars:append {
-  stl = " ",
+    stl = " ",
 }
 vim.opt.fillchars:append "fold:•"
 
 vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
@@ -69,9 +69,9 @@ vim.cmd [[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatop
 -- vim.cmd[[set formatoptions-=cro]]
 
 vim.filetype.add {
-  extension = {
-    conf = "dosini",
-  },
+    extension = {
+        conf = "dosini",
+    },
 }
 
 vim.cmd [[autocmd TermOpen * setlocal signcolumn=no]]
