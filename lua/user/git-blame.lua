68493fa5d4ddb0b75ebfdeefea02ba66e16ffbed
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-07-15 10:10:13 +0530

    Disable lsp_signature in favor of blink.cmp's signature

--- lua/user/git-blame.lua
+++ lua/user/git-blame.lua
@@ -1,4 +1,4 @@
-vim.g.gitblame_enabled = 1
+vim.g.gitblame_enabled = 0
 -- vim.g.gitblame_message_template = "<summary> • <date> • <author>"
 -- vim.g.gitblame_highlight_group = "LineNr"
 -- vim.g.gitblame_date_format = '%r'
