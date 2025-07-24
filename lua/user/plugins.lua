4574df2466445f87e12b4c9d3bb54e74a34c2985
shrimali <Kushashwa.Shrimali@deshaw.com>
2025-07-24 17:56:22 +0530

    Move to codecompanion + remove nerd icons

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -478,4 +478,133 @@ require("lazy").setup {
         event = "InsertEnter",
         config = true
     },
+    {
+        "krshrimali/nvim-utils.nvim",
+        config = function()
+            require("tgkrsutil").setup({
+                enable_test_runner = true,
+                test_runner = function(file, func)
+                    return string.format("pytest %s -k %s", file, func)
+                end,
+            })
+        end,
+        event = "VeryLazy",
+    },
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+            { "nvim-lua/plenary.nvim" },
+            -- Test with blink.cmp (delete if not required)
+            -- {
+            --     "saghen/blink.cmp",
+            --     lazy = false,
+            --     version = "*",
+            --     opts = {
+            --         keymap = {
+            --             preset = "enter",
+            --             ["<S-Tab>"] = { "select_prev", "fallback" },
+            --             ["<Tab>"] = { "select_next", "fallback" },
+            --         },
+            --         cmdline = { sources = { "cmdline" } },
+            --         sources = {
+            --             default = { "lsp", "path", "buffer", "codecompanion" },
+            --         },
+            --     },
+            -- },
+            -- Test with nvim-cmp
+            -- { "hrsh7th/nvim-cmp" },
+        },
+        opts = {
+            --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+            strategies = {
+                --NOTE: Change the adapter as required
+                chat = { adapter = "copilot" },
+                inline = { adapter = "copilot" },
+            },
+            opts = {
+                log_level = "DEBUG",
+            },
+        },
+    },
+    -- {
+    --     "yetone/avante.nvim",
+    --     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    --     -- ⚠️ must add this setting! ! !
+    --     build = function()
+    --         -- conditionally use the correct build system for the current OS
+    --         if vim.fn.has("win32") == 1 then
+    --             return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    --         else
+    --             return "make"
+    --         end
+    --     end,
+    --     event = "VeryLazy",
+    --     version = false, -- Never set this value to "*"! Never!
+    --     ---@module 'avante'
+    --     ---@type avante.Config
+    --     opts = {
+    --         -- add any opts here
+    --         -- for example
+    --         provider = "copilot",
+    --         providers = {
+    --             claude = {
+    --                 endpoint = "https://api.anthropic.com",
+    --                 model = "claude-sonnet-4-20250514",
+    --                 timeout = 30000, -- Timeout in milliseconds
+    --                 extra_request_body = {
+    --                     temperature = 0.75,
+    --                     max_tokens = 20480,
+    --                 },
+    --             },
+    --             moonshot = {
+    --                 endpoint = "https://api.moonshot.ai/v1",
+    --                 model = "kimi-k2-0711-preview",
+    --                 timeout = 30000, -- Timeout in milliseconds
+    --                 extra_request_body = {
+    --                     temperature = 0.75,
+    --                     max_tokens = 32768,
+    --                 },
+    --             },
+    --         },
+    --     },
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "MunifTanjim/nui.nvim",
+    --         --- The below dependencies are optional,
+    --         "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    --         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    --         "hrsh7th/nvim-cmp",      -- autocompletion for avante commands and mentions
+    --         "ibhagwan/fzf-lua",      -- for file_selector provider fzf
+    --         "stevearc/dressing.nvim", -- for input provider dressing
+    --         "folke/snacks.nvim",     -- for input provider snacks
+    --         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    --         "zbirenbaum/copilot.lua", -- for providers='copilot'
+    --         {
+    --             -- support for image pasting
+    --             "HakonHarnes/img-clip.nvim",
+    --             event = "VeryLazy",
+    --             opts = {
+    --                 -- recommended settings
+    --                 default = {
+    --                     embed_image_as_base64 = false,
+    --                     prompt_for_file_name = false,
+    --                     drag_and_drop = {
+    --                         insert_mode = true,
+    --                     },
+    --                     -- required for Windows users
+    --                     use_absolute_path = true,
+    --                 },
+    --             },
+    --         },
+    --         {
+    --             -- Make sure to set this up properly if you have lazy=true
+    --             'MeanderingProgrammer/render-markdown.nvim',
+    --             opts = {
+    --                 file_types = { "markdown", "Avante" },
+    --             },
+    --             ft = { "markdown", "Avante" },
+    --         },
+    --     },
+    -- }
 }
