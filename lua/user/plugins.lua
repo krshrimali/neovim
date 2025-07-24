c3ed81e6a44f79b17827b68fa61b73f61175a64d
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-07-24 21:22:17 +0530

    Use lspsaga for breadcrumbs - both in lualine and top bar

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -599,4 +599,13 @@ require("lazy").setup {
     --         },
     --     },
     -- }
+    {
+        'nvimdev/lspsaga.nvim',
+        config = function()
+            require('lspsaga').setup({})
+        end,
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter', -- optional
+        }
+    }
 }
