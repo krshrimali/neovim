216bf946b09635b4cb0cbe5a558c52482dc540da
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-07-26 18:55:31 +0530

    Remove avante + disable git blame by default

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -3,16 +3,7 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
--- if not config_status_ok then
---   return
--- end
-
 local icons = require "user.icons"
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-local notify = require "nvim-tree.notify"
 
 --
 -- This function has been generated from your
