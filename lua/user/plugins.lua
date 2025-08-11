0135dacc495c4601938c92df3c52de5063a899f7
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-08-11 15:49:01 +0530

    Gitbrowse fixes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -510,13 +510,16 @@ require("lazy").setup {
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
             bigfile = { enabled = true },
+            gitbrowse = {
+                what = "permalink",
+            },
             dashboard = { enabled = false }, -- Completely disable dashboard
             explorer = { enabled = false },
             indent = { enabled = false },
             input = { enabled = false },
             picker = { enabled = false },   -- Disable picker since we're using nvim-tree
             notifier = { enabled = false }, -- Disable for faster startup
-            quickfile = { enabled = true },
+            quickfile = { enabled = false },
             scope = { enabled = false },
             scroll = { enabled = false },
             statuscolumn = { enabled = false }, -- Disable for faster startup
