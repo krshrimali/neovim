bea6717c49b8c25b3a2d35bba6a44d6dae3fd909
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-08-15 04:43:41 +0000

    Move default theme to nvim's default; cursor-dark goes tadadadada

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,21 +1,23 @@
+-- RELY ON DEFAULT THEME AS THE DEFAULT
 -- Cursor Dark Theme Configuration
 -- This is a custom theme inspired by Cursor IDE's dark theme
 
 -- Load the Cursor Dark theme
-local cursor_theme = require("user.themes.init")
+-- local cursor_theme = require("user.themes.init")
+--
+-- -- Setup the theme
+-- local status_ok, _ = pcall(cursor_theme.setup, {
+--   -- Options can be added here in the future
+--   -- transparent = true,
+--   -- italic_comments = true,
+-- })
+--
+-- if not status_ok then
+--   vim.notify("Failed to load Cursor Dark theme, falling back to default")
+--   vim.cmd("colorscheme default")
+--   return
+-- end
 
--- Setup the theme
-local status_ok, _ = pcall(cursor_theme.setup, {
-  -- Options can be added here in the future
-  -- transparent = false,
-  -- italic_comments = true,
-})
-
-if not status_ok then
-  vim.notify("Failed to load Cursor Dark theme, falling back to default")
-  vim.cmd("colorscheme default")
-  return
-end
 
 -- Alternative: You can also load other themes by uncommenting below
 -- local colorscheme = "darkplus"
