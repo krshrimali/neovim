5ad388a2e427d31a5734f66290681f2f273783b4
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-08-17 12:08:29 +0000

    Fix blink.cmp config (important) and fix deprecation error in diagnostics

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -76,13 +76,6 @@ vim.diagnostic.config {
   },
 }
 
--- Configure diagnostic signs
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 -- Python: Pyright for LSP features (navigation, completion, hover)
 lspconfig.pyright.setup {
   on_attach = on_attach,
