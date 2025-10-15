0cd3aeb0835fff4b5c580ec4dc95fa307e33f7ea
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-10-15 22:25:06 +0530

    Enable focus in documentation in coc

--- lua/user/coc.lua
+++ lua/user/coc.lua
@@ -80,11 +80,36 @@ keyset("n", "gi", "<Plug>(coc-implementation)", { silent = true })
 keyset("n", "gr", "<Plug>(coc-references)", { silent = true })
 
 -- Use K to show documentation in preview window
+-- Press K again to focus the documentation window
+-- Press q to close it
 function _G.show_docs()
   local cw = vim.fn.expand "<cword>"
   if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
     vim.api.nvim_command("h " .. cw)
   elseif vim.api.nvim_eval "coc#rpc#ready()" then
+    -- Get all floating windows from CoC
+    local has_float = vim.fn["coc#float#has_float"]()
+
+    if has_float == 1 then
+      -- If float window exists, try to jump to it
+      local winids = vim.fn["coc#float#get_float_win_list"]()
+      if winids and #winids > 0 then
+        -- Find the first visible floating window
+        for _, winid in ipairs(winids) do
+          if vim.api.nvim_win_is_valid(winid) then
+            vim.api.nvim_set_current_win(winid)
+            -- Set up q to close when in the float window
+            local bufnr = vim.api.nvim_win_get_buf(winid)
+            vim.keymap.set("n", "q", function()
+              vim.api.nvim_win_close(winid, true)
+            end, { buffer = bufnr, silent = true, noremap = true, nowait = true })
+            return
+          end
+        end
+      end
+    end
+
+    -- No float window exists, show hover
     vim.fn.CocActionAsync "doHover"
   else
     vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
