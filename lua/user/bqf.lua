a6a6c0d92168629a750dada2682cd6dfaf25f284
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2026-03-12 14:44:38 +0530

    fixes in quickfix list, just use alt+q

--- lua/user/bqf.lua
+++ lua/user/bqf.lua
@@ -4,14 +4,10 @@ if not status_ok then return end
 bqf.setup {
   auto_enable = true,
   auto_resize_height = true,
-  -- Disable magic window so QF+preview use a stable layout; prevents preview
-  -- float from overlapping the editor when QF is in a side split.
-  magic_window = false,
   border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
   preview = {
     auto_preview = true,
-    wrap = true,
-    -- Opaque preview so any remaining overlap doesn’t show editor content through.
+    wrap = false,
     winblend = 0,
   },
 }
