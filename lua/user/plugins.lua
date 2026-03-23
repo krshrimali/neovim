2e951a225b9b1009f534ebf63cc3b31ec76103de
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2026-03-23 21:36:38 +0530

    fix keymaps

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -357,54 +357,6 @@ require("lazy").setup({
     config = function() require "user.whichkey" end,
   },
 
-  {
-    'dmtrKovalenko/fff.nvim',
-    build = function()
-      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-      require("fff.download").download_or_build_binary()
-    end,
-    -- if you are using nixos
-    -- build = "nix run .#release",
-    opts = {              -- (optional)
-      debug = {
-        enabled = true,   -- we expect your collaboration at least during the beta
-        show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
-      },
-    },
-    -- No need to lazy-load with lazy.nvim.
-    -- This plugin initializes itself lazily.
-    lazy = false,
-    keys = {
-      {
-        "ff", -- try it if you didn't it is a banger keybinding for a picker
-        function() require('fff').find_files() end,
-        desc = 'FFFind files',
-      },
-      {
-        "fg",
-        function() require('fff').live_grep() end,
-        desc = 'LiFFFe grep',
-      },
-      {
-        "fz",
-        function()
-          require('fff').live_grep({
-            grep = {
-              modes = { 'fuzzy', 'plain' }
-            }
-          })
-        end,
-        desc = 'Live fffuzy grep',
-      },
-      {
-        "fc",
-        function() require('fff').live_grep({ query = vim.fn.expand("<cword>") }) end,
-        desc = 'Search current word',
-      },
-    }
-  },
-
   -- ============================================
   -- AI: Copilot
   -- ============================================
@@ -691,15 +643,15 @@ require("lazy").setup({
   },
 
   { "nvim-mini/mini.map", version = false },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
+  -- {
+  --   "MeanderingProgrammer/render-markdown.nvim",
+  --   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  --   ---@module 'render-markdown'
+  --   ---@type render.md.UserConfig
+  --   opts = {},
+  -- },
 
   -- Goto Preview (LSP definition/reference previews)
   {
